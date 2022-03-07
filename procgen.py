ab693005dd9b43ac2e3741f988790ff795382e67
Gardego5 <garrett.davis.7777@gmail.com>
2022-03-07 07:42:14 -0800

    Implemented choosing sites for placing enitities

--- procgen.py
+++ procgen.py
@@ -5,10 +5,33 @@ from typing import Tuple, Callable, Iterator, Iterable, TYPE_CHECKING
 if TYPE_CHECKING:
     from entity import Entity
 
+import random
+
 import tcod
 
 from game_map import GameMap
 import generated_structures
+
+
+def place_entities(
+    room: Room,
+    dungeon: GameMap,
+    max_monsters: int,
+    min_monsters: int = 0,
+) -> None:
+    number_of_monsters = random.randint(min_monsters, max_monsters)
+
+    for i in range(number_of_monsters):
+        while i < room.area:  # Choose location for new entity
+            x = random.randint(room.x1, room.x2)
+            y = random.randint(room.y1, room.y2)
+            # Increment counter if entity already at chosen site to prevent infinite loop.
+            if any(entity.x == x and entity.y == y for entity in dungeon.entities): i += 1
+            # If no entity already at chosen site and site is walkable, break loop and use chosen site
+            elif dungeon.tiles[x, y]["walkable"]: break
+
+        # TODO place entities at chosen site.
+        
 
 
 def generate_dungeon(
