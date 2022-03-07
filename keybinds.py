4c7162f2d16bf6d6a59c5163dadfea1c87789e35
Gardego5 <garrett.davis.7777@gmail.com>
2022-03-07 07:15:21 -0800

    added numpad keybinds

--- keybinds.py
+++ keybinds.py
@@ -1,20 +1,23 @@
-from glob import escape
 import tcod.event
 
 up = {
     tcod.event.K_UP,
+    tcod.event.K_KP_8,
 }
 
 down = {
     tcod.event.K_DOWN,
+    tcod.event.K_KP_2,
 }
 
 left = {
     tcod.event.K_LEFT,
+    tcod.event.K_KP_4,
 }
 
 right = {
     tcod.event.K_RIGHT,
+    tcod.event.K_KP_6,
 }
 
 escape = {
