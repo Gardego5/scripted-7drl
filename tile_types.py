2d4fe41e0656aa7d37c15f681757b1b66600d10b
Gardego5 <garrett.davis.7777@gmail.com>
2022-03-07 13:40:10 -0800

    Changed Colors

--- tile_types.py
+++ tile_types.py
@@ -38,11 +38,11 @@ SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 # Tiles
 floor = new_tile(
     walkable=True, transparent=True, 
-    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
-    light=(ord(" "), (255, 255, 255), (200, 180, 50)),
+    dark=(ord(" "), (255, 255, 255), (90, 90, 90)),
+    light=(ord(" "), (255, 255, 255), (180, 180, 210)),
 )
 wall = new_tile(
     walkable=False, transparent=False, 
-    dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
-    light=(ord(" "), (255, 255, 255), (130, 110, 50)),
+    dark=(ord(" "), (255, 255, 255), (60, 60, 60)),
+    light=(ord(" "), (255, 255, 255), (80, 80, 120)),
 )
