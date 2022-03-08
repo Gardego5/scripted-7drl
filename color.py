93000730d9dcb59927fe67afaa4c767b8e8f67f8
Gardego5 <garrett.davis.7777@gmail.com>
2022-03-08 15:11:47 -0800

    cleaned up color.py and added some colors

--- color.py
+++ color.py
@@ -1,22 +1,29 @@
-white                   = 0xFF, 0xFF, 0xFF
-gray_85                 = 0xD9, 0xD9, 0xD9
-gray_75                 = 0xBF, 0xBF, 0xBF
-gray_50                 = 0x80, 0x80, 0x80
-gray_25                 = 0x40, 0x40, 0x40
-gray_15                 = 0x26, 0x26, 0x26
-black                   = 0x00, 0x00, 0x00
+#       Basic Colors        # ####  ####  ####
+white                       = 0xFF, 0xFF, 0xFF
+gray_85                     = 0xD9, 0xD9, 0xD9
+gray_75                     = 0xBF, 0xBF, 0xBF
+gray_50                     = 0x80, 0x80, 0x80
+gray_25                     = 0x40, 0x40, 0x40
+gray_15                     = 0x26, 0x26, 0x26
+black                       = 0x00, 0x00, 0x00
 
-ui_main                 = gray_85
-ui_subdued              = gray_50
+#         UI Colors         # ####  ####  ####
+ui_main                     = gray_85
+ui_subdued                  = gray_50
+welcome_text                = 0x20, 0xA0, 0xFF
+invalid                     = 0xFF, 0xFF, 0x00
+impossible                  = gray_50
+error                       = 0xFF, 0x40, 0x40
 
-player_atk              = 0xE0, 0xE0, 0xE0
-enemy_atk               = 0xFF, 0xC0, 0xC0
+hp_bar_text                 = black
+hp_bar_filled               = 0xE1, 0xF0, 0x9E
+hp_bar_empty                = 0xFC, 0xFF, 0xF0
 
-player_die              = 0xFF, 0x30, 0x30
-enemy_die               = 0xFF, 0xA0, 0x30
+#       Status Colors       # ####  ####  ####
+player_atk                  = 0xE0, 0xE0, 0xE0
+enemy_atk                   = 0xFF, 0xC0, 0xC0
 
-welcome_text            = 0x20, 0xA0, 0xFF
+player_die                  = 0xFF, 0x30, 0x30
+enemy_die                   = 0xFF, 0xA0, 0x30
 
-hp_bar_text             = black
-hp_bar_filled           = 0xE1, 0xF0, 0x9E
-hp_bar_empty            = 0xFC, 0xFF, 0xF0
+health_recovered            = 0x00, 0xFF, 0x00
