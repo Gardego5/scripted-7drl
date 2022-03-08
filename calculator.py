0af05b685feedc0f515660fb1ca8a223111c3bb8
Gardego5 <garrett.davis.7777@gmail.com>
2022-03-07 18:59:08 -0800

    added tuple itemwise subtraction method

--- calculator.py
+++ calculator.py
@@ -2,3 +2,6 @@ from typing import Tuple, Any
 
 def tuple_add(*tuples) -> Tuple[Any]:
     return tuple(map(sum, zip(*tuples)))
+
+def tuple_subtract(t1, t2) -> Tuple[Any]:
+    return tuple(map(sum, zip(t1, map(lambda x: -x, t2))))
