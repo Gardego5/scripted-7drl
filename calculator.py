8ce6e2f72cd6cef663ec78e930909abc8a33a6b5
Gardego5 <garrett.davis.7777@gmail.com>
2022-03-10 15:31:41 -0800

    random direction calculator

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 from typing import Tuple, Any
 import math
+import random
 
 def tuple_add(*tuples) -> Tuple[Any]:
     return tuple(map(sum, zip(*tuples)))
@@ -9,3 +10,11 @@ def tuple_subtract(t1, t2) -> Tuple[Any]:
 
 def tuple_distance(t1, t2) -> float:
     return math.sqrt((t1[0] - t2[0]) ** 2 + (t1[1] - t2[1]) ** 2)
+
+def random_direction() -> Tuple[int, int]:
+    dirs = [
+        (-1, -1), ( 0, -1), ( 1, -1),
+        (-1,  0), ( 0,  0), ( 1,  0),
+        (-1,  1), ( 0,  1), ( 1,  1),
+    ]
+    return random.choice(dirs)
